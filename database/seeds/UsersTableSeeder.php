f1e2de1fb118ecb3ca662a1bfbf8c5e5f3e31c19
ShineD-166 <ShineD-166@domain.wgs.local>
2021-04-09 10:31:54 +0530

    make seed for owner

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,7 +19,7 @@ class UsersTableSeeder extends Seeder
         DB::table('roles')->delete();
         $usercount = DB::table('users')->where('email', 'admin@gmail.com')->count();
         // Role::create(['name' => 'User']);
-        Role::create(['name' => 'Owner']);
+        // Role::create(['name' => 'Owner']);
         if($usercount == 0){
             $user = User::create([
                 'first_name' => 'Venue',
@@ -60,6 +60,28 @@ class UsersTableSeeder extends Seeder
             $role->syncPermissions($permissions);
 
             $customer->assignRole([$role->id]);
+        }
+
+        //create Owner role
+        $ownerecount = DB::table('users')->where('email', 'owner@gmail.com')->count();
+        if($ownerecount == 0){
+            $owner = User::create([
+                'first_name' => 'owner',
+                'last_name' => 'owner',
+                'email' => 'owner@gmail.com',
+                'password' => bcrypt('123456'),
+                'social_type' => 'Website',
+                'social_id' => 0,
+                'status' => 1,
+                'created_at' => date('Y-m-d H:i:s'),
+            ]);
+            $role = Role::create(['name' => 'Owner']);
+
+            $permissions = Permission::pluck('id','id')->all();
+
+            $role->syncPermissions($permissions);
+
+            $owner->assignRole([$role->id]);
         }
     }
 }
